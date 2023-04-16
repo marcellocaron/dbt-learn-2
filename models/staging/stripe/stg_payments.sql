7c7f7a52b5ecf728bad98e1f0cdbac8fc757461a
marcello.caron@gmail.com <marcello.caron@gmail.com>
2023-04-16 19:33:15 +0000

    macros

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
-    amount / 100 as amount,
+    status, 
+    {{- cents_to_dollars('amount')}} as amount,
     created
 from {{ source('stripe', 'payment') }}
